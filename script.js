7386206c3b3621506dd92c6e09fdbbc6b1d678e6
Yash Kulkarni <yashreadytobox@gmail.com>
2023-10-09 18:41:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-// Define a function named 'openSocket' for WebSocket communication.
-openSocket = () => {
-    // Create a new WebSocket connection to the server at "ws://127.0.0.1:9997/".
-    socket = new WebSocket("ws://127.0.0.1:9997/");
+let videoSocket; // WebSocket connection for video
+let numericSocket; // WebSocket connection for numeric data
+
+// Define a function to open a WebSocket connection for video.
+openVideoSocket = () => {
+    // Create a new WebSocket connection to the server for video at "ws://127.0.0.1:9997/video".
+    videoSocket = new WebSocket("ws://127.0.0.1:9997/video");
     
     // Get the HTML element with id "msg" and store it in the 'msg' variable.
     let msg = document.getElementById("msg");
 
-    // Add an event listener for when the WebSocket connection is opened.
-    socket.addEventListener('open', (e) => {
-        // Update the HTML element with id "status" to indicate that the connection is opened.
-        document.getElementById("status").innerHTML = "Opened";
+    // Add an event listener for when the WebSocket connection for video is opened.
+    videoSocket.addEventListener('open', (e) => {
+        // Update the HTML element with id "status" to indicate that the video connection is opened.
+        document.getElementById("status").innerHTML = "Video Connection Opened";
     });
 
-    // Add an event listener for when a message is received from the WebSocket server.
-    socket.addEventListener('message', (e) => {
+    // Add an event listener for when a message is received from the video WebSocket server.
+    videoSocket.addEventListener('message', (e) => {
         // Get the 2D rendering context of the "msg" element.
         let ctx = msg.getContext("2d");
 
@@ -30,3 +33,34 @@ openSocket = () => {
         });
     });
 }
+
+// Define a function to open a WebSocket connection for numeric data.
+openNumericSocket = () => {
+    // Create a new WebSocket connection to the server for numeric data at "ws://127.0.0.1:9997/numeric".
+    numericSocket = new WebSocket("ws://127.0.0.1:9997/numeric");
+    
+    // Add an event listener for when the WebSocket connection for numeric data is opened.
+    numericSocket.addEventListener('open', (e) => {
+        // Update the HTML element with id "status" to indicate that the numeric data connection is opened.
+        document.getElementById("status").innerHTML = "Numeric Data Connection Opened";
+    });
+}
+
+// Define a function to send numeric data to the server.
+sendNumericData = () => {
+    // Get the numeric data from the input field with id "numericData".
+    let numericDataInput = document.getElementById("numericData");
+    let numericData = numericDataInput.value;
+
+    // Send the numeric data to the server using the numeric data WebSocket connection.
+    if (numericSocket.readyState === WebSocket.OPEN) {
+        numericSocket.send(numericData);
+    }
+}
+
+// Call the functions to open WebSocket connections when the page loads.
+window.onload = () => {
+    openVideoSocket();
+    openNumericSocket();
+}
+
